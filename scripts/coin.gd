abf83d8425bb1b009ba03eed64f7120e6163f866
Vic-41148 <zorozamarimo@gmail.com>
2025-10-19 16:48:13 +0530

    Finished layour and animations, added score count

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var game_manager: Node = %GameManager
+
 #
 ## Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
@@ -12,5 +14,5 @@ extends Area2D
 	#pass
 
 func _on_body_exited(body: Node2D) -> void:
-	print("+1 coin")
+	game_manager.add_point()
 	queue_free()
